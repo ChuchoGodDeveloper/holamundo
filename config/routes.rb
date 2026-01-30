3d52bfc391d28eefbd48396b1ceda3590b18bee5
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-01-29 19:34:11 -0600

    Regresando

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
 
   # 3. Ruta para tu pantalla final (Hola Mundo + Ticket)
-  # Le ponemos el nombre 'inicio' para poder llamarla después
   get 'inicio', to: 'welcome#index', as: 'inicio'
 
-  # 4. Rutas para el Formulario de Registro
-  get 'registro', to: 'registros#new', as: 'new_registro'
-  post 'registros', to: 'registros#create' # Acción estándar REST
+  # 4. SOLUCIÓN MÁGICA:
+  # 'resources' crea automáticamente la ruta 'show' (para ver el perfil)
+  # y 'update' (para subir las fotos después). Esto arregla tu error 500.
+  resources :registros
 end
