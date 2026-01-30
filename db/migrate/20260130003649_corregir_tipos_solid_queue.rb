61d34c7e5973adba78d0f0e43a6bc9f992198ed7
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-01-29 18:43:40 -0600

    Corregir migracion con using timestamp

--- db/migrate/20260130003649_corregir_tipos_solid_queue.rb
+++ db/migrate/20260130003649_corregir_tipos_solid_queue.rb
@@ -1,13 +1,13 @@
 class CorregirTiposSolidQueue < ActiveRecord::Migration[8.0]
   def change
-    # Cambiamos las columnas de Integer a Datetime (Fecha y Hora)
-    change_column :solid_queue_jobs, :scheduled_at, :datetime
-    change_column :solid_queue_jobs, :finished_at, :datetime
+    # Ahora le decimos a Postgres explícitamente CÓMO convertir los datos usando 'to_timestamp'
+    change_column :solid_queue_jobs, :scheduled_at, :datetime, using: 'to_timestamp(scheduled_at)'
+    change_column :solid_queue_jobs, :finished_at, :datetime, using: 'to_timestamp(finished_at)'
     
-    change_column :solid_queue_scheduled_executions, :scheduled_at, :datetime
+    change_column :solid_queue_scheduled_executions, :scheduled_at, :datetime, using: 'to_timestamp(scheduled_at)'
     
-    change_column :solid_queue_blocked_executions, :expires_at, :datetime
+    change_column :solid_queue_blocked_executions, :expires_at, :datetime, using: 'to_timestamp(expires_at)'
     
-    change_column :solid_queue_semaphores, :expires_at, :datetime
+    change_column :solid_queue_semaphores, :expires_at, :datetime, using: 'to_timestamp(expires_at)'
   end
 end
